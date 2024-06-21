e4ddead480e6e6fcaf36f57f18b30fb9f92c8569
timas <timas3811@gmail.com>
2024-06-22 00:04:54 +0200

    add functional for Timer component

--- components/Timer.js
+++ components/Timer.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useCallback } from 'react';
 import {
     StyleSheet,
     View,
@@ -8,13 +9,10 @@ import {
     Platform,
     Button,
 } from 'react-native';
-
 import { Picker } from '@react-native-picker/picker';
-import {useNavigation} from "@react-navigation/native";
-
+import { useNavigation } from '@react-navigation/native';
 
 const screen = Dimensions.get('window');
-
 
 const styles = StyleSheet.create({
     container: {
@@ -78,18 +76,122 @@ const styles = StyleSheet.create({
 });
 
 function TimerComponent() {
+    const [remainingSeconds, setRemainingSeconds] = useState(5);
+    // Создает состояние для отслеживания оставшихся секунд таймера. Изначально установлено в 5 секунд.
+    const [isRunning, setIsRunning] = useState(false);
+    // Создает состояние для отслеживания, запущен ли таймер. Изначально таймер не запущен.
+    const [selectedMinutes, setSelectedMinutes] = useState('0');
+    // Создает состояние для отслеживания выбранных минут. Изначально установлено в '0'.
+    const [selectedSeconds, setSelectedSeconds] = useState('5');
+    // Создает состояние для отслеживания выбранных секунд. Изначально установлено в '5'.
     const navigation = useNavigation();
+
+    useEffect(() => {
+        // useEffect обрабатывает логику таймера.
+        let interval;
+        // Если таймер запущен, создает интервал, который уменьшает remainingSeconds каждую секунду.
+        if (isRunning) {
+            interval = setInterval(() => {
+                setRemainingSeconds(prev => prev - 1);
+            }, 1000);
+        }
+        // Останавливает таймер, если время истекло.
+        if (remainingSeconds === 0 && isRunning) {
+            clearInterval(interval);
+            setIsRunning(false);
+        }
+        // Очищает интервал при размонтировании компонента или изменении зависимостей.
+        return () => clearInterval(interval);
+    }, [isRunning, remainingSeconds]);
+
+    // Запускает таймер с выбранным временем.
+    const start = useCallback(() => {
+        setRemainingSeconds(
+            parseInt(selectedMinutes, 10) * 60 + parseInt(selectedSeconds, 10)
+        );
+        setIsRunning(true);
+    }, [selectedMinutes, selectedSeconds]);
+
+    // Останавливает таймер.
+    const stop = useCallback(() => {
+        setIsRunning(false);
+    }, []);
+
+    // Функция для отрисовки выбора минут и секунд в виде выпадающих списков.
+    const renderPickers = () => {
+        const AVAILABLE_MINUTES = createArray(10);
+        const AVAILABLE_SECONDS = createArray(60);
+
+        return (
+
+            <View style={styles.pickerContainer}>
+
+                <Picker
+                    style={styles.picker}
+                    itemStyle={styles.pickerItem}
+                    selectedValue={selectedMinutes}
+                    onValueChange={(itemValue) => setSelectedMinutes(itemValue)}
+                    mode='dropdown'
+                >
+                    {AVAILABLE_MINUTES.map((value) => (
+                        <Picker.Item key={value} label={value} value={value} />
+                    ))}
+                </Picker>
+                <Text style={styles.pickerItem}>minutes</Text>
+                <Picker
+                    style={styles.picker}
+                    itemStyle={styles.pickerItem}
+                    selectedValue={selectedSeconds}
+                    onValueChange={(itemValue) => setSelectedSeconds(itemValue)}
+                    mode='dropdown'
+                >
+                    {AVAILABLE_SECONDS.map((value) => (
+                        <Picker.Item key={value} label={value} value={value} />
+                    ))}
+                </Picker>
+                <Text style={styles.pickerItem}>seconds</Text>
+
+            </View>
+        );
+    };
+
+
+    // Функция форматирования числа, добавляет ведущий ноль, если число меньше 10.
+    const formatNumber = (number) => `0${number}`.slice(-2);
+
+    // Функция, вычисляющая оставшееся время в минутах и секундах на основе переданного времени в секундах.
+    const getRemaining = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time - minutes * 60;
+        return { minutes: formatNumber(minutes), seconds: formatNumber(seconds) };
+    };
+
+// Функция, создающая массив чисел от 0 до заданной длины в виде строк.
+    const createArray = (length) => {
+        return Array.from({ length }, (_, i) => i.toString());
+    };
+// Получение отформатированных минут и секунд на основе оставшегося времени таймера.
+    const { minutes, seconds } = getRemaining(remainingSeconds);
+
     return (
-        <View style={styles.pickerContainer}>
-            <Text style={styles.pickerItem}>Timer Page</Text>
-            <Button
-                title="Перейти к будильнику"
-                onPress={() => navigation.navigate('Alarm')}
-            />
+        <View style={styles.container}>
+            <StatusBar barStyle='light-content' />
+            {isRunning ? (
+                <Text style={styles.timerText}>{`${minutes}:${seconds}`}</Text>
+            ) : (
+                renderPickers()
+            )}
+            {isRunning ? (
+                <TouchableOpacity onPress={stop} style={[styles.button, styles.buttonStop]}>
+                    <Text style={[styles.buttonText, styles.buttonTextStop]}>Stop</Text>
+                </TouchableOpacity>
+            ) : (
+                <TouchableOpacity onPress={start} style={styles.button}>
+                    <Text style={styles.buttonText}>Start</Text>
+                </TouchableOpacity>
+            )}
         </View>
-
     );
-
 }
 
 export default TimerComponent;
